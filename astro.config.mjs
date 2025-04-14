196a635c95926e1a72ac1ec6d2292c0d5f28baa4
Staslios <>
2025-04-14 14:10:32 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/Staslios/grateful-giant',
+    site: 'https://staslios.github.io',
+    base: '/grateful-giant',
     integrations: [mdx(), sitemap(), react(), partytown()],
 });
